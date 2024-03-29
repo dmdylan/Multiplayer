0614433c3aee0b0f9701be37cc096af46e048f10
dmdylan <51095343+dmdylan@users.noreply.github.com>
2024-03-28 23:48:36 -0400

    Player character selection

--- Scripts/PlayerInfo.cs
+++ Scripts/PlayerInfo.cs
@@ -1,18 +1,27 @@
 using Godot;
 using System;
 
+public enum PlayerCharacterClass { Warrior, Mage, Cleric, Rogue}
+
 public class PlayerInfo
 {
 	private readonly int id;
 	private readonly string name;
+	private PlayerCharacterClass playerCharacterClass;
 
 	public int ID => id;
 	public string Name => name;
+	public PlayerCharacterClass PlayerCharacterClass => playerCharacterClass;
 
 	public PlayerInfo(int id, string name)
 	{
 		this.id = id;
 		this.name = name;
-
+	}
+	
+	public void ChangePlayerCharacterClass(PlayerCharacterClass playerCharacterClass)
+	{
+		this.playerCharacterClass = playerCharacterClass;
 	}
 }
+
