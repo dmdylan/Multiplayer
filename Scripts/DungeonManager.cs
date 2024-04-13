021ffd90b5cc36ebc7ba91dc3d3d64535c511ed7
dmdylan <51095343+dmdylan@users.noreply.github.com>
2024-04-12 21:35:13 -0400

    Dungeon tile selection

--- Scripts/DungeonManager.cs
+++ Scripts/DungeonManager.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime;
 using Godot;
 
 public partial class DungeonManager : Node
--- Scripts/DungeonTileNode.cs
+++ Scripts/DungeonTileNode.cs
@@ -1,10 +1,47 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class DungeonTileNode : Panel
 {
+	[Export] private TextureButton textureButton;
+	[Export] private GridContainer iconGrid;
+
+	private List<TextureRect> imageTextures;
+
+	public override void _Ready()
+	{
+		foreach (var item in iconGrid.GetChildren(false))
+		{
+			GD.Print(item.Name);
+			if(item is TextureRect rect)
+				imageTextures.Add(rect);
+		}
+	}
+
+	public override void _EnterTree()
+	{
+		textureButton.Pressed += OnDungeonTileButtonPressed;
+	}
+
+	public override void _ExitTree()
+	{
+		textureButton.Pressed -= OnDungeonTileButtonPressed;
+	}
+
 	private void OnDungeonTileButtonPressed()
 	{
-		GD.Print("Pressed");
+		Rpc(nameof(SetIconOnTile), Multiplayer.GetUniqueId());
+	}
+	
+	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
+	private void SetIconOnTile(int id)
+	{
+		TextureRect textureRect = imageTextures.Where(x => x.Texture == null).First();
+		
+		textureRect.Texture = ImageTexture.CreateFromImage(GameManager.Instance.Players[id].Entity.EntityInfo.EntityIcon);
+		
+		textureRect.Visible = true;
 	}
 }
