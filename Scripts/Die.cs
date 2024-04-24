b913da1dfa056a5f773c3516a48af9ce209d66d0
dmdylan <dylandanowski@gmail.com>
2024-04-24 12:17:22 -0400

    Moving dice roll logic to dicemanager

--- Scripts/Die.cs
+++ Scripts/Die.cs
@@ -4,42 +4,6 @@ public enum DieType { D4, D6, D8}
 
 public partial class Die : RigidBody3D
 {
-	[Export] private Sprite3D[] faces;
+	[Export] public Sprite3D[] Faces { get; private set; }
 	[Export] public DieType DieType { get; private set; } = DieType.D6;
-
-	private bool hasCheckedSides = false;
-
-	public override void _EnterTree()
-	{
-		SleepingStateChanged += SleepingStateChangedEventHandler;
-	}
-
-	public override void _ExitTree()
-	{
-		SleepingStateChanged -= SleepingStateChangedEventHandler;
-	}
-
-	//TODO: Can just move all of this logic to the dicemanager, let it check there and just use Die as a fancy data container possibly
-	private async void SleepingStateChangedEventHandler()
-	{
-		float topHeight = 0;
-		int index = 0;
-
-		for (int i = 0; i < faces.Length; i++)
-		{
-			if (faces[i].GlobalTransform.Origin.Y > topHeight)
-			{
-				topHeight = faces[i].GlobalTransform.Origin.Y;
-				index = i;
-			}
-		}
-
-		GD.Print(index);
-		hasCheckedSides = true;
-		GD.Print($"Top face is {faces[index].Name}");
-
-		await ToSignal(GetTree().CreateTimer(.5f), "timeout");
-
-		QueueFree();
-	}
 }
--- Scripts/Managers/DiceManager.cs
+++ Scripts/Managers/DiceManager.cs
@@ -6,45 +6,95 @@ public partial class DiceManager : Node
 {
 	private static DiceManager instance;
 	public static DiceManager Instance => instance;
-	
+
 	[Export] private PackedScene d6Debug;
-	
+
+	private List<Die> currentDice = new();
+
 	public override void _EnterTree()
 	{
-		if(instance != null)
+		if (instance != null)
 			QueueFree();
 		else
 			instance = this;
 	}
-	
+
 	//TODO: Will need to sync the dice roll
 	//TODO: Dice can get stuck sometimes, might need to reroll. Can use raycast on each side to check it one is touching the ground
 	public void RollDice(List<Die> dice)
 	{
 		RandomNumberGenerator random = new();
-		
+
 		for (int i = 0; i < dice.Count; i++)
 		{
 			Die die = GetDieTypeScene(dice[i]).Instantiate<Die>();
-			
-			die.Translate(new Vector3(random.RandfRange(-3f,3f),5, random.RandfRange(-3f,3f)));
-			
+
+			currentDice.Add(die);
+
+			die.SleepingStateChanged += DieSleepingStateChangedEventHandler;
+		}
+
+		foreach (var die in currentDice)
+		{
+			die.Translate(new Vector3(random.RandfRange(-3f, 3f), 5, random.RandfRange(-3f, 3f)));
+
 			float randomX = random.RandfRange(-Mathf.Pi, Mathf.Pi);
 			float randomY = random.RandfRange(-Mathf.Pi, Mathf.Pi);
 			float randomZ = random.RandfRange(-Mathf.Pi, Mathf.Pi);
-			
-			
+
+
 			die.Rotation = new Vector3(randomX, randomY, randomZ);
-			
-			AddChild(die);		
-					
+
+			AddChild(die);
+
 			die.ApplyCentralImpulse(die.GlobalTransform.Basis.Z * 5);
 		}
-		
+
 		//TODO: Move the deletion and handling of dice out of die script and to dice manager
 		//Wait for all die to finish
 	}
-	
+
+	private void DieSleepingStateChangedEventHandler()
+	{
+		foreach (var die in currentDice)
+		{
+			if (!die.Sleeping)
+				return;
+		}
+		
+		foreach (var die in currentDice)
+		{
+			CheckDieTopSide(die);
+		}
+		
+		foreach (var die in currentDice)
+		{
+			die.SleepingStateChanged -= DieSleepingStateChangedEventHandler;
+			die.QueueFree();
+		}
+		
+		currentDice.Clear();
+		//TODO: Maybe remove trimexcess
+		currentDice.TrimExcess();
+	}
+
+	private void CheckDieTopSide(Die die)
+	{
+		float topHeight = 0;
+		int index = 0;
+
+		for (int i = 0; i < die.Faces.Length; i++)
+		{
+			if (die.Faces[i].GlobalTransform.Origin.Y > topHeight)
+			{
+				topHeight = die.Faces[i].GlobalTransform.Origin.Y;
+				index = i;
+			}	
+		}
+		
+		GD.Print($"Top face is {die.Faces[index].Name}");
+	}
+
 	private PackedScene GetDieTypeScene(Die die)
 	{
 		switch (die.DieType)
